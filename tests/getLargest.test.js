0c6aa7a14f03cbed6e504c1b83dc38e8877f0531
CallumCheng <callumcheng@outlook.com>
2022-07-12 11:27:18 +0100

    refactored test.js

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -14,8 +14,8 @@ describe("getLargest", () => {
             [[1,2,3], 3],
             [[9,8,7], 9],
             [[12,36,19], 36],
-        ]).test("", (arr, expected) => {
-            expect(getLargest(...arr)).toEqual(expected)
+        ]).test("%s -> %s", (arr, expected) => {
+            expect(getLargest(...arr)).toEqual(expected) //'...' is spread operator - used to expand/spread an array/iterator
 
         })
    })
